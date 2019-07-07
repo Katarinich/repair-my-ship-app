eaf2499e967ac815c3cbad97f12b443914b9898a
Katarinich <katarina.chirich@gmail.com>
2019-07-07 15:15:37 +0300

    Added server

---
+++ server/index.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const { graphqlExpress, graphiqlExpress } = require('apollo-server-express');
+const { makeExecutableSchema } = require('graphql-tools');
+
+const books = [
+  {
+    title: "Harry Potter and the Sorcerer's stone",
+    author: 'J.K. Rowling',
+  },
+  {
+    title: 'Jurassic Park',
+    author: 'Michael Crichton',
+  },
+];
+
+const typeDefs = `
+  type Query { books: [Book] }
+  type Book { title: String, author: String }
+`;
+
+const resolvers = {
+  Query: { books: () => books },
+};
+
+const schema = makeExecutableSchema({
+  typeDefs,
+  resolvers,
+});
+
+const app = express();
+
+app.use('/', (req, res) => {
+  res.send('Hello World!');
+})
+
+app.use('/graphql', bodyParser.json(), graphqlExpress({ schema }));
+
+app.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }));
+
+app.listen(3001, () => {
+  console.log('Go to http://localhost:3001/graphiql to run queries!');
+});
