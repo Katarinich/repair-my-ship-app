b0e3035789d4b1497f72b27ebf9dbb06b6f19f61
Katarinich <katarina.chirich@gmail.com>
2019-07-21 11:55:34 +0300

    Renamed App to app and added post component

---
+++ client/src/components/app/app.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import Post from "../post";
+
+import "./app.scss";
+
+function App() {
+  return (
+    <div className="app">
+      <Post />
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/post/post.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+import "./post.scss";
+
+const Post = () => (
+  <div class="post">
+    <div class="post__title">A Post Title</div>
+    <div class="post__content">A Post Content</div>
+  </div>
+);
+
+export default Post;
