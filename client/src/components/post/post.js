2d1ff5cedae9696f2620e1c8eae5fa6d0fdd6a64
Katarinich <katarina.chirich@gmail.com>
2019-09-01 08:46:44 +0100

    Changed class to className in post component

--- client/src/components/post/post.js
+++ client/src/components/post/post.js
@@ -3,9 +3,9 @@ import React from "react";
 import "./post.scss";
 
 const Post = () => (
-  <div class="post">
-    <div class="post__title">A Post Title</div>
-    <div class="post__content">A Post Content</div>
+  <div className="post">
+    <div className="post__title">A Post Title</div>
+    <div className="post__content">A Post Content</div>
   </div>
 );
 
