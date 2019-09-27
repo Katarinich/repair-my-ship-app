18d5b7337c24187450574fc2f2e495117e598087
Katarinich <katarina.chirich@gmail.com>
2019-09-27 12:39:28 +0300

    Added workbox

--- client/src/serviceWorker.js
+++ client/src/serviceWorker.js
@@ -32,7 +32,7 @@ export function register(config) {
     }
 
     window.addEventListener('load', () => {
-      const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
+      const swUrl = `${process.env.PUBLIC_URL}/sw.js`;
 
       if (isLocalhost) {
         // This is running on localhost. Let's check if a service worker still exists or not.
@@ -55,6 +55,7 @@ export function register(config) {
 }
 
 function forceSWupdate() {
+  console.log('forceSWupdate')
   if ('serviceWorker' in navigator) {
     navigator.serviceWorker.getRegistrations().then(function(registrations) {
       for (let registration of registrations) {
